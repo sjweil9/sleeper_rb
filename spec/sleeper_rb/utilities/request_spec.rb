7a0cb6f32b3cb2a5b8e950c35ea2128891e0fb0d
Stephen Weil <stephen.weil@gmail.com>
2021-09-06 23:04:20 -0500

    Rubo

--- spec/sleeper_rb/utilities/request_spec.rb
+++ spec/sleeper_rb/utilities/request_spec.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
+RESPONSE = Struct.new(:code, :body)
+
 RSpec.describe SleeperRb::Utilities::Request do
   describe "#execute_request" do
-    RESPONSE = Struct.new(:code, :body)
-    let(:response_200) { RESPONSE.new("200", { foo: "bar" }.to_json) }
-    let(:response_404) { RESPONSE.new("404") }
-    let(:response_400) { RESPONSE.new("400") }
-    let(:response_429) { RESPONSE.new("429") }
-    let(:response_500) { RESPONSE.new("500") }
+    let(:response200) { RESPONSE.new("200", { foo: "bar" }.to_json) }
+    let(:response404) { RESPONSE.new("404") }
+    let(:response400) { RESPONSE.new("400") }
+    let(:response429) { RESPONSE.new("429") }
+    let(:response500) { RESPONSE.new("500") }
 
     let(:uri) { URI(url) }
     let(:url) { SleeperRb::Utilities::Request::BASE_URL }
@@ -20,35 +21,35 @@ RSpec.describe SleeperRb::Utilities::Request do
 
     context "when response is 200" do
       it "should return the parsed JSON" do
-        expect(Net::HTTP).to receive(:get_response).with(uri).and_return(response_200)
+        expect(Net::HTTP).to receive(:get_response).with(uri).and_return(response200)
         expect { subject.send(:execute_request, url) }.not_to raise_error
       end
     end
 
     context "when response is 404" do
       it "should raise NotFound" do
-        expect(Net::HTTP).to receive(:get_response).with(uri).and_return(response_404)
+        expect(Net::HTTP).to receive(:get_response).with(uri).and_return(response404)
         expect { subject.send(:execute_request, url) }.to raise_error(SleeperRb::NotFound)
       end
     end
 
     context "when response is 400" do
       it "should raise BadRequest" do
-        expect(Net::HTTP).to receive(:get_response).with(uri).and_return(response_400)
+        expect(Net::HTTP).to receive(:get_response).with(uri).and_return(response400)
         expect { subject.send(:execute_request, url) }.to raise_error(SleeperRb::BadRequest)
       end
     end
 
     context "when response is 429" do
       it "should raise RateLimitExceeded" do
-        expect(Net::HTTP).to receive(:get_response).with(uri).and_return(response_429)
+        expect(Net::HTTP).to receive(:get_response).with(uri).and_return(response429)
         expect { subject.send(:execute_request, url) }.to raise_error(SleeperRb::RateLimitExceeded)
       end
     end
 
     context "when response is any other code" do
       it "should raise ServerError" do
-        expect(Net::HTTP).to receive(:get_response).with(uri).and_return(response_500)
+        expect(Net::HTTP).to receive(:get_response).with(uri).and_return(response500)
         expect { subject.send(:execute_request, url) }.to raise_error(SleeperRb::ServerError)
       end
     end
